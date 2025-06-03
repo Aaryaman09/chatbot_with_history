9c7b8222cfbd31e2e0bbc880033eb3eadd3c154f
Aaryaman Sharma <aaryaman09@gmail.com>
2025-06-03 15:54:53 +0530

    added classes in the code, made it better

--- standalone_app.py
+++ standalone_app.py
@@ -1,18 +1,40 @@
 import json, os
 from langchain_core.messages import HumanMessage
 from langchain_core.runnables.history import RunnableWithMessageHistory
-from utils import fetch_llm, get_config, get_session_history, track_queries_on_langsmith
+from langchain_community.chat_message_histories import ChatMessageHistory
+from langchain_core.chat_history import BaseChatMessageHistory
+from utils import fetch_llm, get_config, track_queries_on_langsmith
 
-pass_keys = get_config("key.json")
-track_queries_on_langsmith(flag=pass_keys.get("track_queries_on_langsmith", False))
+class ChatBotOllama:
+    """
+    A simple chatbot that uses Ollama or Groq.
+    """
+    def __init__(self, pass_keys: dict=get_config("key.json")):
+        self.pass_keys = pass_keys
+        self.store = {}
+        # Initialize tracking on LangSmith 
+        track_queries_on_langsmith(flag=pass_keys.get("track_queries_on_langsmith", False))
 
-# fetching the LLM from utils
-llm, model_name = fetch_llm(pass_keys["llm_service"], pass_keys.get("GROQ_API_KEY"))
+    def get_session_history(self, session_id:str) -> BaseChatMessageHistory:
+        """
+        Get the session chat message history.
+        """
+        if session_id not in self.store:
+            self.store[session_id] = ChatMessageHistory()
+        return self.store[session_id]
 
-with_message_history = RunnableWithMessageHistory(llm, get_session_history)
+    def fetch_message_history_runnable(self):
+        # fetching the LLM from utils
+        llm, model_name = fetch_llm(self.pass_keys["llm_service"], self.pass_keys.get("GROQ_API_KEY"))
+
+        with_message_history = RunnableWithMessageHistory(llm, self.get_session_history)
+
+        return with_message_history
 
 if __name__ == "__main__":
-    # Define the function to handle the translation
+    chatbot = ChatBotOllama()
+    with_message_history = chatbot.fetch_message_history_runnable()
+
     while True:
         user_input = input("You: ")
         if user_input.lower() == "exit":
--- utils/__init__.py
+++ utils/__init__.py
@@ -1,7 +1,5 @@
 
 from langchain_ollama import ChatOllama as Ollama
-from langchain_community.chat_message_histories import ChatMessageHistory
-from langchain_core.chat_history import BaseChatMessageHistory
 # from langchain_community.llms import Ollama
 from langchain_groq import ChatGroq
 import json, os
@@ -14,15 +12,6 @@ def track_queries_on_langsmith(flag: bool = False):
         os.environ["LANGCHAIN_API_KEY"] = pass_keys["LANGCHAIN_API_KEY"]
         os.environ["LANGCHAIN_TRACING_V2"] = pass_keys["LANGCHAIN_TRACING_V2"]
         os.environ["LANGCHAIN_PROJECT"] = pass_keys["LANGCHAIN_PROJECT_NAME"]
-
-
-def get_session_history(session_id:str) -> BaseChatMessageHistory:
-    """
-    Get the session chat message history.
-    """
-    if session_id not in store:
-        store[session_id] = ChatMessageHistory()
-    return store[session_id]
 
 def fetch_llm(inference_type: str, groq_api_key: str = "") -> tuple:
     if inference_type == "paid":
