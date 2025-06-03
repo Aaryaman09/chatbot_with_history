65176a22863d28f2584b023ba67f801529307d6c
Aaryaman Sharma <aaryaman09@gmail.com>
2025-06-03 16:18:03 +0530

    Added chatprompttemplate with language translation capability

--- standalone_app.py
+++ standalone_app.py
@@ -3,6 +3,7 @@ from langchain_core.messages import HumanMessage
 from langchain_core.runnables.history import RunnableWithMessageHistory
 from langchain_community.chat_message_histories import ChatMessageHistory
 from langchain_core.chat_history import BaseChatMessageHistory
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from utils import fetch_llm, get_config, track_queries_on_langsmith
 
 class ChatBotOllama:
@@ -25,15 +26,24 @@ class ChatBotOllama:
 
     def fetch_message_history_runnable(self):
         # fetching the LLM from utils
+
+        prompt = ChatPromptTemplate.from_messages([
+            ("system", "You are a helpful assistant. Answer the user's questions to the best of your ability in language : {language}."),
+            MessagesPlaceholder(variable_name="messages"),
+        ])
+
         llm, model_name = fetch_llm(self.pass_keys["llm_service"], self.pass_keys.get("GROQ_API_KEY"))
 
-        with_message_history = RunnableWithMessageHistory(llm, self.get_session_history)
+        chain = prompt | llm
 
-        return with_message_history
+        with_message_history = RunnableWithMessageHistory(chain, self.get_session_history, input_messages_key="messages")
+
+        return with_message_history, model_name
 
 if __name__ == "__main__":
     chatbot = ChatBotOllama()
-    with_message_history = chatbot.fetch_message_history_runnable()
+    with_message_history, model_name = chatbot.fetch_message_history_runnable()
+    print(f"Welcome to the ChatBot : {model_name}! Type 'exit' to quit.")
 
     while True:
         user_input = input("You: ")
@@ -48,7 +58,13 @@ if __name__ == "__main__":
         }
 
         # Invoke the LLM with the human message
-        response = with_message_history.invoke([HumanMessage(content=user_input)], config=configuration)
+        response = with_message_history.invoke(
+            {
+                "messages": [HumanMessage(content=user_input)],
+                "language": key_config.get("language", "English")
+            },
+            config=configuration
+        )
         print("Assistant:", response.content)
-        
+
         del key_config
